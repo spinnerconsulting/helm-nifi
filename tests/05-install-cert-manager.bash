6a7ed356fe82d8f976b7b3c5f97abb1e1d84afa7
Gregory M. Foreman <gforeman@spinnerconsulting.com>
2024-11-12 12:29:32 -0500

    fix tests

--- tests/05-install-cert-manager.bash
+++ tests/05-install-cert-manager.bash
@@ -8,10 +8,8 @@ sudo apt-get install -y golang-go
 OS=$(go env GOOS)
 ARCH=$(go env GOARCH)
 
-/bin/rm -rf /tmp/cmctl-install
-mkdir -p /tmp/cmctl-install
-
-curl -L -o /tmp/cmctl-install/cmctl.tar.gz https://github.com/jetstack/cert-manager/releases/latest/download/cmctl-$OS-$ARCH.tar.gz
-(cd /tmp/cmctl-install ; tar xvzf cmctl.tar.gz ; sudo mv cmctl /usr/local/bin)
+curl -fsSL -o cmctl https://github.com/cert-manager/cmctl/releases/latest/download/cmctl_${OS}_${ARCH}
+chmod +x cmctl
+sudo mv cmctl /usr/local/bin
 
 cmctl experimental install
